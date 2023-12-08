4c2e9c4ce3c7e0980d4416fe68a3c2949951b6c4
Chirag Malhotra <cmalhotradelhi@gmail.com>
2023-12-07 19:09:24 -0500

    Testing basic packet parsing

---
+++ FinalProject/source/server/source/test/test.d
@@ -0,0 +1,28 @@
+module server.source.test.test;
+
+import server.source.packet.packet;
+import server.source.packet.client_packet;
+import server.source.packet.deserialize_client;
+import std.stdio;
+
+
+unittest
+{
+    ClientPacket p;
+    p.client_id = 'A';
+    p.move_num = 5;
+
+    char[ClientPacket.sizeof] buffer = p.serialize;
+    ClientPacket ans = deserialize(buffer);
+    assert(ans.client_id == 'A');
+}
+unittest
+{
+    ClientPacket p;
+    p.client_id = 'A';
+    p.move_num = 5;
+
+    char[ClientPacket.sizeof] buffer = p.serialize;
+    ClientPacket ans = deserialize(buffer);
+    assert( ans.move_num == 5);
+}
